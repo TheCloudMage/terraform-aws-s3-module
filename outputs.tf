282869eddcd2adb583531191675cdd4ae5f05e4d
Rich Nason <rnason@cloudmage.io>
2020-01-22 13:54:10 -0500

    Fixed Outputs reference to un-encrypted bucket instead of non-encrypted bucket

--- outputs.tf
+++ outputs.tf
@@ -2,17 +2,17 @@
 # S3 Bucket Outputs: #
 ######################
 output "s3_bucket_id" {
-  value = var.s3_encryption_enabled == true ? aws_s3_bucket.encrypted_bucket[0].id : aws_s3_bucket.not_encrypted_bucket[0].id
+  value = var.s3_encryption_enabled == true ? aws_s3_bucket.encrypted_bucket[0].id : aws_s3_bucket.un_encrypted_bucket[0].id
 }
 
 output "s3_bucket_arn" {
-  value = var.s3_encryption_enabled == true ? aws_s3_bucket.encrypted_bucket[0].arn : aws_s3_bucket.not_encrypted_bucket[0].arn
+  value = var.s3_encryption_enabled == true ? aws_s3_bucket.encrypted_bucket[0].arn : aws_s3_bucket.un_encrypted_bucket[0].arn
 }
 
 output "s3_bucket_domain_name" {
-  value = var.s3_encryption_enabled == true ? aws_s3_bucket.encrypted_bucket[0].bucket_domain_name : aws_s3_bucket.not_encrypted_bucket[0].bucket_domain_name
+  value = var.s3_encryption_enabled == true ? aws_s3_bucket.encrypted_bucket[0].bucket_domain_name : aws_s3_bucket.un_encrypted_bucket[0].bucket_domain_name
 }
 
 output "s3_bucket_region" {
-  value = var.s3_encryption_enabled == true ? aws_s3_bucket.encrypted_bucket[0].region : aws_s3_bucket.not_encrypted_bucket[0].region
+  value = var.s3_encryption_enabled == true ? aws_s3_bucket.encrypted_bucket[0].region : aws_s3_bucket.un_encrypted_bucket[0].region
 }
